ecc019cc445668e940a9a26d277bfbe859764a84
si0005hp <si0005hp@gmail.com>
2019-05-04 00:09:30 +0900

    TblDir basic read method

--- fs.go
+++ fs.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/hanwen/go-fuse/fuse"
@@ -17,7 +20,16 @@ type Root struct {
 // TblDir ...
 type TblDir struct {
 	nodefs.Node
-	tbl string
+	db     *sql.DB
+	tbl    string
+	pkCols []string
+}
+
+// RowFile ...
+type RowFile struct {
+	nodefs.Node
+	id   string // id represents the file name same time
+	data []byte
 }
 
 func newRoot(db *sql.DB) *Root {
@@ -65,6 +77,7 @@ func (root *Root) OpenDir(ctx *fuse.Context) ([]fuse.DirEntry, fuse.Status) {
 		// Create TblDir
 		tblDir := &TblDir{
 			Node: nodefs.NewDefaultNode(),
+			db:   root.db,
 			tbl:  tbl,
 		}
 		root.Inode().NewChild(tbl, true, tblDir)
@@ -79,4 +92,63 @@ func (root *Root) Lookup(out *fuse.Attr, name string, ctx *fuse.Context) (*nodef
 		return nil, fuse.ENOENT
 	}
 	return c, fuse.OK
+}
+
+/* TblDir */
+
+// GetAttr ...
+func (dir *TblDir) GetAttr(out *fuse.Attr, file nodefs.File, ctx *fuse.Context) fuse.Status {
+	out.Mode = fuse.S_IFDIR | 0755
+	return fuse.OK
+}
+
+// OpenDir ...
+func (dir *TblDir) OpenDir(ctx *fuse.Context) ([]fuse.DirEntry, fuse.Status) {
+	// Fetch tables rows
+	rows, err := Rows(dir.db, dir.tbl)
+	if err != nil {
+		panic(err)
+	}
+	cols, err := rows.Columns()
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	// Create childs
+	rowIdx := 1
+	var dirs []fuse.DirEntry
+	for rows.Next() {
+		// Scan row
+		vals := make([]interface{}, len(cols))
+		for i := range vals {
+			var s string
+			vals[i] = &s
+		}
+		err = rows.Scan(vals...)
+		if err != nil {
+			panic(err)
+		}
+		// Create file content
+		lines := make([]string, len(cols))
+		for i, p := range vals {
+			v := *(p.(*string))
+			lines[i] = fmt.Sprintf("%s=%s", cols[i], v)
+		}
+		// Add child
+		fileNm := strconv.Itoa(rowIdx)
+		file := &RowFile{
+			Node: nodefs.NewDefaultNode(),
+			id:   fileNm,
+			data: []byte(strings.Join(lines, "\n")),
+		}
+		dir.Inode().NewChild(fileNm, false, file)
+		// Add DirEntry
+		dirs = append(dirs, fuse.DirEntry{
+			Mode: fuse.S_IFREG | 0755,
+			Name: fileNm,
+		})
+		rowIdx++
+	}
+	return dirs, fuse.OK
 }
