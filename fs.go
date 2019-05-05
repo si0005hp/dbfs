867dc518596b8dde4c9418921c0e81a688f2bebb
si0005hp <si0005hp@gmail.com>
2019-05-05 10:41:30 +0900

    Refactor fs.go 2

--- fs.go
+++ fs.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"os"
 	"reflect"
@@ -14,31 +15,37 @@ import (
 	"github.com/magiconair/properties"
 )
 
+// Node ...
+type Node interface {
+	nodefs.Node
+	GetFileInfo() *FileInfo
+	GetChildren() ([]Node, error)
+}
+
 // Root ...
 type Root struct {
 	nodefs.Node
-	FileInfo
-	con DBCon
+	fileInfo FileInfo
+	con      DBCon
 }
 
 // TblDir ...
 type TblDir struct {
 	nodefs.Node
-	FileInfo
-	con  DBCon
-	meta *TblMeta
+	fileInfo FileInfo
+	con      DBCon
+	meta     *TblMeta
 }
 
 // RowFile ...
 type RowFile struct {
 	nodefs.Node
-	FileInfo
-	id     string // id represents the file name same time
-	data   []byte
-	parent *TblDir // parent TblDir
+	fileInfo FileInfo
+	data     []byte
+	parent   *TblDir // parent TblDir
 }
 
-// FileInfo ... refs os.FileInfo
+// FileInfo ... @see os.FileInfo
 type FileInfo struct {
 	name         string
 	size         int64
@@ -47,7 +54,6 @@ type FileInfo struct {
 	creationTime time.Time
 }
 
-// IsDir ... refs os.FileMode#IsDir
 func (i *FileInfo) isDir() bool {
 	return i.mode&os.ModeDir != 0
 }
@@ -77,27 +83,13 @@ func newRoot(con DBCon) *Root {
 	now := time.Now()
 	return &Root{
 		Node: nodefs.NewDefaultNode(),
-		FileInfo: FileInfo{
+		fileInfo: FileInfo{
 			mode:         os.ModeDir | 0755,
 			modTime:      now,
 			creationTime: now,
 		},
 		con: con,
 	}
-}
-
-func (root *Root) mount(mtpt string, isDbg bool) error {
-	opts := nodefs.Options{
-		AttrTimeout:  time.Second,
-		EntryTimeout: time.Second,
-		Debug:        isDbg,
-	}
-	s, _, err := nodefs.MountRoot(mtpt, root, &opts)
-	if err != nil {
-		return err
-	}
-	s.Serve()
-	return nil
 }
 
 func lookup(n nodefs.Node, out *fuse.Attr, name string, ctx *fuse.Context) (*nodefs.Inode, fuse.Status) {
@@ -116,26 +108,67 @@ func lookup(n nodefs.Node, out *fuse.Attr, name string, ctx *fuse.Context) (*nod
 	return c, fuse.OK
 }
 
+func openDir(n Node) ([]fuse.DirEntry, fuse.Status) {
+	children, err := n.GetChildren()
+	if err != nil {
+		panic(err)
+	}
+	dirs := make([]fuse.DirEntry, len(children))
+	for i, c := range children {
+		fi := c.GetFileInfo()
+		fi.mapDirEntry(&dirs[i])
+		if n.Inode().GetChild(fi.name) == nil {
+			n.Inode().NewChild(fi.name, fi.isDir(), c)
+		}
+	}
+	return dirs, fuse.OK
+}
+
 /* Root */
+
+func (root *Root) mount(mtpt string, isDbg bool) error {
+	opts := nodefs.Options{
+		AttrTimeout:  time.Second,
+		EntryTimeout: time.Second,
+		Debug:        isDbg,
+	}
+	s, _, err := nodefs.MountRoot(mtpt, root, &opts)
+	if err != nil {
+		return err
+	}
+	s.Serve()
+	return nil
+}
 
 // GetAttr ...
 func (root *Root) GetAttr(out *fuse.Attr, file nodefs.File, ctx *fuse.Context) fuse.Status {
-	root.FileInfo.mapAttr(out)
+	root.GetFileInfo().mapAttr(out)
 	return fuse.OK
 }
 
-func (root *Root) getChildren() ([]*TblDir, error) {
+// OpenDir ...
+func (root *Root) OpenDir(ctx *fuse.Context) ([]fuse.DirEntry, fuse.Status) {
+	return openDir(root)
+}
+
+// Lookup ...
+func (root *Root) Lookup(out *fuse.Attr, name string, ctx *fuse.Context) (*nodefs.Inode, fuse.Status) {
+	return lookup(root, out, name, ctx)
+}
+
+// GetChildren ...
+func (root *Root) GetChildren() ([]Node, error) {
 	now := time.Now()
 	ms, err := root.con.GetTblsMetadata()
 	if err != nil {
 		return nil, err
 	}
 	i := 0
-	dirs := make([]*TblDir, len(ms))
+	dirs := make([]Node, len(ms))
 	for tbl, m := range ms {
 		dirs[i] = &TblDir{
 			Node: nodefs.NewDefaultNode(),
-			FileInfo: FileInfo{
+			fileInfo: FileInfo{
 				name:         tbl,
 				mode:         os.ModeDir | 0755,
 				modTime:      now,
@@ -149,32 +182,16 @@ func (root *Root) getChildren() ([]*TblDir, error) {
 	return dirs, nil
 }
 
-// OpenDir ...
-func (root *Root) OpenDir(ctx *fuse.Context) ([]fuse.DirEntry, fuse.Status) {
-	children, err := root.getChildren()
-	if err != nil {
-		panic(err)
-	}
-	dirs := make([]fuse.DirEntry, len(children))
-	for i, c := range children {
-		c.FileInfo.mapDirEntry(&dirs[i])
-		if root.Inode().GetChild(c.FileInfo.name) == nil {
-			root.Inode().NewChild(c.FileInfo.name, true, c)
-		}
-	}
-	return dirs, fuse.OK
-}
-
-// Lookup ...
-func (root *Root) Lookup(out *fuse.Attr, name string, ctx *fuse.Context) (*nodefs.Inode, fuse.Status) {
-	return lookup(root, out, name, ctx)
+// GetFileInfo ...
+func (root *Root) GetFileInfo() *FileInfo {
+	return &root.fileInfo
 }
 
 /* TblDir */
 
 // GetAttr ...
 func (dir *TblDir) GetAttr(out *fuse.Attr, file nodefs.File, ctx *fuse.Context) fuse.Status {
-	dir.FileInfo.mapAttr(out)
+	dir.GetFileInfo().mapAttr(out)
 	return fuse.OK
 }
 
@@ -183,7 +200,13 @@ func (dir *TblDir) Lookup(out *fuse.Attr, name string, ctx *fuse.Context) (*node
 	return lookup(dir, out, name, ctx)
 }
 
-func (dir *TblDir) getChildren() ([]*RowFile, error) {
+// OpenDir ...
+func (dir *TblDir) OpenDir(ctx *fuse.Context) ([]fuse.DirEntry, fuse.Status) {
+	return openDir(dir)
+}
+
+// GetChildren ...
+func (dir *TblDir) GetChildren() ([]Node, error) {
 	rows, err := dir.con.FetchRows(dir.meta.tblNm)
 	if err != nil {
 		return nil, err
@@ -195,7 +218,7 @@ func (dir *TblDir) getChildren() ([]*RowFile, error) {
 	defer rows.Close()
 
 	rowIdx := 1
-	files := make([]*RowFile, 0)
+	files := make([]Node, 0)
 	for rows.Next() {
 		// Scan row
 		vals := make([]interface{}, len(cols))
@@ -218,14 +241,13 @@ func (dir *TblDir) getChildren() ([]*RowFile, error) {
 		data := []byte(strings.Join(lines, "\n"))
 		files = append(files, &RowFile{
 			Node: nodefs.NewDefaultNode(),
-			FileInfo: FileInfo{
+			fileInfo: FileInfo{
 				name:         fileNm,
 				size:         int64(len(data)),
 				mode:         0666,
-				modTime:      dir.FileInfo.modTime,
-				creationTime: dir.FileInfo.creationTime,
+				modTime:      dir.GetFileInfo().modTime,
+				creationTime: dir.GetFileInfo().creationTime,
 			},
-			id:     fileNm,
 			data:   data,
 			parent: dir,
 		})
@@ -234,33 +256,32 @@ func (dir *TblDir) getChildren() ([]*RowFile, error) {
 	return files, nil
 }
 
-// OpenDir ...
-func (dir *TblDir) OpenDir(ctx *fuse.Context) ([]fuse.DirEntry, fuse.Status) {
-	children, err := dir.getChildren()
-	if err != nil {
-		panic(err)
-	}
-	dirs := make([]fuse.DirEntry, len(children))
-	for i, c := range children {
-		c.FileInfo.mapDirEntry(&dirs[i])
-		if dir.Inode().GetChild(c.FileInfo.name) == nil {
-			dir.Inode().NewChild(c.FileInfo.name, false, c)
-		}
-	}
-	return dirs, fuse.OK
+// GetFileInfo ...
+func (dir *TblDir) GetFileInfo() *FileInfo {
+	return &dir.fileInfo
 }
 
 /* RowFile */
 
 // GetAttr ...
 func (f *RowFile) GetAttr(out *fuse.Attr, file nodefs.File, ctx *fuse.Context) fuse.Status {
-	f.FileInfo.mapAttr(out)
+	f.GetFileInfo().mapAttr(out)
 	return fuse.OK
 }
 
 // Open ...
 func (f *RowFile) Open(flags uint32, ctx *fuse.Context) (nodefs.File, fuse.Status) {
 	return nodefs.NewDataFile(f.data), fuse.OK
+}
+
+// GetChildren ...
+func (f *RowFile) GetChildren() ([]Node, error) {
+	return nil, errors.New("Unsupported operation")
+}
+
+// GetFileInfo ...
+func (f *RowFile) GetFileInfo() *FileInfo {
+	return &f.fileInfo
 }
 
 // Write ...
